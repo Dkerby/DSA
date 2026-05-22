e3dc4bd5f4143e153b8f206c723229dc2da29d73
Drew Kerby <14812961+Dkerby@users.noreply.github.com>
2026-05-22 11:15:22 -0500

    Add: binary-tree-from-preorder-and-inorder-traversal - submission-0

---
+++ Data Structures & Algorithms/binary-tree-from-preorder-and-inorder-traversal/submission-0.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        if len(preorder) == 0:
+            return None 
+
+        root = TreeNode(preorder[0])
+
+        for i in range(0, len(inorder)):
+            if root.val == inorder[i]:
+                root.left = self.buildTree(preorder[1: i + 1], inorder[0:i])  
+                root.right = self.buildTree(preorder[i + 1: len(preorder)], inorder[i + 1: len(inorder)])
+
+        return root
