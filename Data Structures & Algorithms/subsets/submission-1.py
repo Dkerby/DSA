21621d1f9d92bb1e082a70f5c234426156f89f04
Drew Kerby <14812961+Dkerby@users.noreply.github.com>
2026-05-25 10:53:10 -0500

    Add: subsets - submission-1

---
+++ Data Structures & Algorithms/subsets/submission-1.py
@@ -0,0 +1,12 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        # start with the empty set
+        arr = [[]]
+
+        # loop over all of the numbers in the list of numbers 
+        for i in range(len(nums)):
+            # look at all of the items currently in the list, and add the new number onto each of them 
+            for j in range(len(arr)):
+                    arr.append(arr[j] + [nums[i]])
+
+        return arr
