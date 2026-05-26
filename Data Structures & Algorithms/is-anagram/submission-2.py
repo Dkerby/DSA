aabcf719f6fb8b64aef8fd3cce38063018d7c309
Drew Kerby <14812961+Dkerby@users.noreply.github.com>
2026-05-25 23:14:11 -0500

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        sMap = {}
+        tMap = {}
+
+        for i in range(len(s)):
+            sVal = s[i]
+            tVal = t[i]
+
+            if sVal not in sMap:
+                sMap[sVal] = 1
+            else:
+                sMap[sVal] += 1
+
+            if tVal not in tMap:
+                tMap[tVal] = 1
+            else:
+                tMap[tVal] += 1
+
+        return sMap == tMap
