ea673c9ffb00c856d2dd29f5517f7de159cc0de1
Drew Kerby <14812961+Dkerby@users.noreply.github.com>
2026-06-09 16:15:36 -0500

    Add: is-palindrome - submission-10

---
+++ Data Structures & Algorithms/is-palindrome/submission-10.py
@@ -0,0 +1,26 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        if len(s) < 2:
+            return True
+
+        forward = 0
+        backward = len(s) - 1
+
+        while forward < backward:
+            while forward < backward and not s[forward].isalnum():
+                forward += 1
+                continue
+
+            while forward < backward and not s[backward].isalnum():
+                backward -= 1
+                continue
+
+            print(s[forward].lower(), s[backward].lower())
+
+            if s[forward].lower() != s[backward].lower():
+                return False
+            
+            forward += 1
+            backward -= 1
+
+        return True
