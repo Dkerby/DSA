4b5f05d36ab76b8240e73ba41867b056e7019aec
Drew Kerby <14812961+Dkerby@users.noreply.github.com>
2026-06-14 23:04:04 -0500

    Add: meeting-schedule - submission-4

---
+++ Data Structures & Algorithms/meeting-schedule/submission-4.rs
@@ -0,0 +1,31 @@
+/**
+ * Definition of Interval:
+ * #[derive(Debug, Clone)]
+ * pub struct Interval {
+ *     pub start: i32,
+ *     pub end: i32,
+ * }
+ *
+ * impl Interval {
+ *     pub fn new(start: i32, end: i32) -> Self {
+ *         Interval { start, end }
+ *     }
+ * }
+ */
+
+impl Solution {
+    pub fn can_attend_meetings(intervals: Vec<Interval>) -> bool {
+        if intervals.len() == 0 {
+            return true
+        }
+        let mut intervals = intervals;
+        intervals.sort_by_key(|i| i.start);
+        for i in 0..intervals.len() - 1 {
+            if intervals[i+1].start < intervals[i].end {
+                return false
+            }
+        }
+
+        return true 
+    }
+}
