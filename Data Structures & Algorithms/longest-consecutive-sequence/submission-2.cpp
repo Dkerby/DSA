6b7ce85f959bc47b7713f404dbc41d4734cebb60
Drew Kerby <14812961+Dkerby@users.noreply.github.com>
2026-07-03 11:01:35 -0500

    Add: longest-consecutive-sequence - submission-2

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-2.cpp
@@ -0,0 +1,29 @@
+class Solution {
+   public:
+    int longestConsecutive(vector<int>& nums) {
+        int count = 0;
+        int maxCount = 0;
+        unordered_set<int> s;
+
+        for (int i = 0; i < nums.size(); i++) {
+            s.insert(nums[i]);
+        }
+
+        int i = 0;
+        for (const auto& curr : s) {
+            if (s.count(curr) && !s.count(curr-1)) {
+                count = 1;
+                int nextNum = curr + 1;
+                while (s.count(nextNum)) {
+                    nextNum++;
+                    count++;
+                }
+                if (count > maxCount) {
+                    maxCount = count;
+                }
+            }
+        }
+
+        return maxCount;
+    }
+};
