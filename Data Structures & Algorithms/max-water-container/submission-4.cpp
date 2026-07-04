80f0bf968af7a74799e408e7029ea08b42117036
Drew Kerby <14812961+Dkerby@users.noreply.github.com>
2026-07-04 12:17:20 -0500

    Add: max-water-container - submission-4

---
+++ Data Structures & Algorithms/max-water-container/submission-4.cpp
@@ -0,0 +1,25 @@
+class Solution {
+   public:
+    int maxArea(vector<int>& heights) {
+        int length = heights.size();
+        int maxArea = 0;
+        int left = 0;
+        int right = length - 1;
+
+        while (left < right) {
+            int hLeft = heights[left];
+            int hRight = heights[right];
+            int area = min(hLeft, hRight) * (right - left);
+
+            maxArea = max(maxArea, area);
+
+            if (hLeft < hRight) {
+                left++;
+            } else {
+                right--;
+            } 
+        }
+
+        return maxArea;
+    }
+};
