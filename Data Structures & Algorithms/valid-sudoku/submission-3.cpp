b3963b630464c1c24c0de86e2c18907646b00e2b
Drew Kerby <14812961+Dkerby@users.noreply.github.com>
2026-07-13 16:50:28 -0500

    Add: valid-sudoku - submission-3

---
+++ Data Structures & Algorithms/valid-sudoku/submission-3.cpp
@@ -0,0 +1,25 @@
+class Solution {
+   public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        int rowSet[9] = {};
+        int colSet[9] = {};
+        int boxSet[9] = {};
+        for (int row = 0; row < 9; ++row) {
+            for (int col = 0; col < 9; ++col) {
+                if (board[row][col] != '.') {
+                    int val = board[row][col] - '1';
+
+                    if ((rowSet[row] & (1 << val)) || (colSet[col] & (1 << val)) ||
+                        (boxSet[(row / 3) * 3 + (col / 3)] & (1 << val))) {
+                        return false;
+                    }
+
+                    rowSet[row] = rowSet[row] | (1 << val);
+                    colSet[col] = colSet[col] | (1 << val);
+                    boxSet[(row / 3) * 3 + (col / 3)] = boxSet[(row / 3) * 3 + (col / 3)] | (1 << val);
+                }
+            }
+        }
+        return true;
+    }
+};
