19eb7fe2127f4d6c913cd5fd4ad6464ef456d54f
Drew Kerby <14812961+Dkerby@users.noreply.github.com>
2026-07-15 14:25:01 -0500

    Add: two-integer-sum-ii - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-1.cpp
@@ -0,0 +1,20 @@
+class Solution {
+   public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int left = 0;
+        int right = numbers.size() - 1;
+
+        while (left < right) {
+            int curSum = numbers[left] + numbers[right];
+            if (curSum < target) {
+                left++;
+            } else if (curSum > target) {
+                right--;
+            } else {
+                return {left + 1, right + 1};
+            }
+        }
+
+        return {};
+    }
+};
