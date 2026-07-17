c8c899e70d63cb4c47f59df1a9301d68edf405cc
Drew Kerby <14812961+Dkerby@users.noreply.github.com>
2026-07-17 15:58:03 -0500

    Add: longest-repeating-substring-with-replacement - submission-3

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-3.cpp
@@ -0,0 +1,29 @@
+class Solution {
+   public:
+    int characterReplacement(string s, int k) {
+        int l = 0;
+        int maxFreq = 0;
+        int maxLen = 0;
+
+        unordered_map<char, int> m;
+        for (int r = 0; r < s.size(); r++) {
+            char leftChar = s[l];
+            char rightChar = s[r];
+            // update the count in the mapping for the current right character
+            m[rightChar]++;
+            maxFreq = max(m[rightChar], maxFreq);
+
+            // if the substring is invalid, move l forward, and update the mapping
+            // a substring is invalid if the number of replacements needed is larger than k
+            while ((r - l + 1) - maxFreq > k) {
+                m[leftChar]--;
+                l++;
+            }
+
+            // update the maxLen with the latest window size
+            maxLen = max(r - l + 1, maxLen);
+        }
+
+        return maxLen;
+    }
+};
