cc505a10f55c535ff6b326c37e725efaeebeba26
Drew Kerby <14812961+Dkerby@users.noreply.github.com>
2026-07-22 16:07:00 -0500

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        vector<int> result(temperatures.size(), 0);
+        int tempSize = temperatures.size();
+
+        for(int i = 0;i < tempSize; i++) {
+            for(int j = i+1;j < tempSize; j++) {
+                cout << temperatures[i] << endl;
+                if(temperatures[j] > temperatures[i]) {
+                    result[i] = j-i;
+                    break;
+                }
+            }
+        } 
+
+        return result;
+    }
+};
